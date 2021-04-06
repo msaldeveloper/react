8f151ed5ac47bd839d374dfb8079889212e637c4
msalda <msalda53@gmail.com>
2021-04-06 00:16:52 -0500

    variables y constantes

---
+++ seccion2_intro_al/intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,15 @@
+//Variables y Constantes
+const nombre = "mario";
+console.log(nombre);
+const apellido = "saldaña";
+console.log(apellido);
+
+let valorDado = 5;
+valorDado = 3;
+console.log(valorDado);
+ if (true){
+     let valorDado = 6;
+     const nombre = "alberto";
+     console.log(valorDado,nombre);
+ }
+
